35871bbfdf6eeaa4f5bde8eb241767f0808a37c1
Michael Navara <michael.navara@arkance-development.com>
2024-08-12 08:12:03 +0200

    Update AssemblyInfo

--- Development/src/InventorAddInDebugger/InventorAddInDebugger/Properties/AssemblyInfo.cs
+++ Development/src/InventorAddInDebugger/InventorAddInDebugger/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 //
 [assembly: AssemblyTitle("InventorAddInDebugger")]
-[assembly: AssemblyDescription("InventorAddInDebugger")]
+[assembly: AssemblyDescription("Tools for debugging Inventor AddIns")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Autodesk, Inc.")]
-[assembly: AssemblyProduct("Autodesk Inventor")]
-[assembly: AssemblyCopyright("Copyright ?2012 Autodesk, Inc.")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Inventor AddIn Debugger")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -29,7 +29,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1.0")]
 
 //
 // In order to sign your assembly you must specify a key to use. Refer to the 
